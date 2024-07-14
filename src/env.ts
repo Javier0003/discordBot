15ec333a97f33a9cf707c5d5d5042d754b93b103
Javier0003 <jpere0500@gmail.com>
2024-07-14 03:43:00 -0400

    New: env variables for mcserver

--- src/env.ts
+++ src/env.ts
@@ -2,11 +2,17 @@ import 'dotenv/config'
 
 const token = process.env.TOKEN as string
 const guildId = process.env.GUILD_ID as string
+const mcIp = process.env.MC_IP as string
+const mcPort = process.env.MC_PORT as string
+const mcPortOpener = Number(process.env.OPENER_PORT) as number
 
 if (!token) throw new Error('No token provided')
 if (!guildId) throw new Error('No guild ID provided')
 
 export default {
   token: token,
-  guildId: guildId
+  guildId: guildId,
+  mcIp: mcIp,
+  mcPort: mcPort,
+  mcPortOpener: mcPortOpener
 }
