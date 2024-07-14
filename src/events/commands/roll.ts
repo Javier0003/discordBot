d350d8e46abdf3fb11e6bbde9c2e655d56fcc607
Javier0003 <jpere0500@gmail.com>
2024-07-14 03:43:24 -0400

    New: roll command

---
+++ src/events/commands/roll.ts
@@ -0,0 +1,23 @@
+import { CacheType, CommandInteraction } from 'discord.js'
+import Command_Builder from '../../structures/command-builder/command-builder'
+
+export default class Roll extends Command_Builder {
+  constructor() {
+    super({
+      name: 'roll',
+      description: 'roll',
+      devOnly: false,
+      testOnly: false,
+      options: [],
+      deleted: false
+    })
+  }
+  public async command(interaction: CommandInteraction<CacheType>) {
+    let random = Math.floor(Math.random() * 100)
+    if (random === 69) {
+      interaction.reply(`😎😎 ${random}`)
+    } else {
+      interaction.reply(`roll ${random}`)
+    }
+  }
+}
