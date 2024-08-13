f4c539dd38592a91f9a77116a9e724e8c6a0d832
Javier0003 <jpere0500@gmail.com>
2024-08-12 20:00:31 -0400

    Updates to db and new table

--- drizzle/schemas/schema.ts
+++ drizzle/schemas/schema.ts
@@ -1,11 +1,21 @@
+import { serial } from 'drizzle-orm/pg-core'
 import { integer, pgTable, varchar,  } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: varchar('id', { length: 256 }).primaryKey(),
-  name: varchar('name', { length: 256 }) ,
-  completados: integer('completados').default(0),
+  name: varchar('name', { length: 256 }),
   osuId: integer('osuId').notNull(),
+  puntos: integer('puntos').notNull().default(0),
 });
+
+export const plays = pgTable('plays', {
+  playId: serial('playId').primaryKey(),
+  mapId: integer('mapId').notNull().references(() => mapas.oldMapId),
+  uId: varchar('uid').notNull().references(() => users.id),
+  rank: varchar('rank').notNull(),
+  score: integer('score').notNull(),
+  accuracy: varchar('accuracy').notNull(),
+})
 
 export const mapas = pgTable('mapas', {
   oldMapId: integer('oldMaps').notNull().primaryKey(),
@@ -15,5 +25,7 @@ export const mapas = pgTable('mapas', {
 
 export type Users = typeof users.$inferSelect
 export type NewUser = typeof users.$inferInsert
+
+export type Plays = typeof plays.$inferSelect
 
 
