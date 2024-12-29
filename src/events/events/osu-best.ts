a7543efd349b01b6dd22a40266bfd6c977552eac
Javier0003 <jpere0500@gmail.com>
2024-12-29 14:33:26 -0400

    command was only working if everyone had a play on the map

--- src/events/events/osu-best.ts
+++ src/events/events/osu-best.ts
@@ -41,9 +41,13 @@ export default class OsuBest extends Event_Builder<'messageCreate'> {
         })
       ) as Welcome[]
 
-      const scores = data.map((v) =>
-        v.scores.filter((f) => f.score === v.scores.reduce((a, b) => a.score > b.score ? a : b).score)
-      )
+      const scores = []
+
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i]
+        if (element.scores.length === 0) continue
+        scores.push(element.scores.filter((f) => f.score === element.scores.reduce((a, b) => a.score > b.score ? a : b).score))
+      }
 
       const sorted = scores.sort((a, b) => b[0].score - a[0].score)
 
@@ -53,9 +57,9 @@ export default class OsuBest extends Event_Builder<'messageCreate'> {
         .setThumbnail(`${content.embeds[0].thumbnail?.url}`)
         .setColor('Random')
 
-      sorted.forEach((v) => {
-        const user = usersData.find((f) => f.osuId === v[0].user_id)
-        embed.addFields([{ name: user?.name || 'Unknown', value: `${v[0].score.toString()} - ${OsuDaily.accuracy(v[0].accuracy)}%` }])
+      sorted.forEach(([v]) => {
+        const user = usersData.find((f) => f.osuId === v.user_id)
+        embed.addFields([{ name: user?.name || 'Unknown', value: `Score: ${v.score.toString()}\nAccuracy: ${OsuDaily.accuracy(v.accuracy)}%\nCombo: ${v.max_combo}x\nRank: ${v.rank}\nMods: ${v.mods.length === 0 ? "no mod" : v.mods}\n` }])
       })
 
       message.channel.send({ embeds: [embed] })
