d8ec72501654ceab6bb177ff1054412e846d1db8
Javier0003 <jpere0500@gmail.com>
2026-01-06 18:54:28 -0400

    mc test

--- src/events/commands/mc-status.ts
+++ src/events/commands/mc-status.ts
@@ -113,7 +113,7 @@ export default class McStatus extends Command {
         //   client.destroy()
         // })
 
-        await fetch("/open/cte2", {
+        await fetch(`http://localhost:${env.mcPortOpener}/open/cte2`, {
           method: "POST"
         });
 
